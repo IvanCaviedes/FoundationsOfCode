2c419d88d4fec92b161fc74e84e21bbd72068bf7
Ivan Caviedes <36604636+IvanCaviedes@users.noreply.github.com>
2023-08-16 13:49:45 -0500

    javascript basic inputs por consola (#5)

---
+++ javascript/Basic/inputs.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+
+// * utilizando readline para crear un input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+async function main() {
+  const Name = await prompt("Como es tu Nombre? ");
+  const Lastname = await prompt("Como es tu Apellido? ");
+  console.log(`Hola ${Name} ${Lastname}`);
+  rl.close();
+}
+
+main();
